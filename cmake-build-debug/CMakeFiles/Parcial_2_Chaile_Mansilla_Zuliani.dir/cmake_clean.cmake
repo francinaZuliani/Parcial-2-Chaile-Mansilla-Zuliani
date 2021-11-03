2b3f5b1c9585daaf56f65edd2fa327b0a97b8cb2
Francina Zuliani <2002300.ucc.edu.ar>
2021-11-03 19:18:04 -0300

    0

---
+++ cmake-build-debug/CMakeFiles/Parcial_2_Chaile_Mansilla_Zuliani.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parcial_2_Chaile_Mansilla_Zuliani.dir/main.cpp.obj"
+  "Parcial_2_Chaile_Mansilla_Zuliani.exe"
+  "Parcial_2_Chaile_Mansilla_Zuliani.exe.manifest"
+  "Parcial_2_Chaile_Mansilla_Zuliani.pdb"
+  "libParcial_2_Chaile_Mansilla_Zuliani.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parcial_2_Chaile_Mansilla_Zuliani.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
