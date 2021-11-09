3c2d07444d613b8f8304f391ecfe0418a922beb4
Francina Zuliani <2002300.ucc.edu.ar>
2021-11-09 00:50:35 -0300

    actualizacion

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "string.h"
+#include <string.h>
 #include <vector>
 #include "Pila.h"
 #include "Lista.h"
 #include "HashMap.h"
 #include <string>
+
+
 
 using namespace std;
 
@@ -29,7 +31,7 @@ void chopCSV(string fileName, int lines)
 
 //estadisticas
 
-void estad(int t, int i, int m)
+void estad(int t, int i, int m, int r10, int r20, int r30, int r40, int r50, int r60, int r70, int r80, int r90, int r100, int r110 )
 {
     float pinfectados, pmuertes;
     cout<<"El total de casos registrados es de "<<t<<endl;
@@ -39,11 +41,17 @@ void estad(int t, int i, int m)
     pmuertes=(m*100)/i;
     cout<<"El porcentaje de infectados por muestra es de %"<<pinfectados<<endl;
     cout<<"El porcentaje de fallecidos por infectados es de %"<<pmuertes<<endl;
+    cout<<"El rango etario de 1 a 10 es de "<<r10<<endl;
+    cout<<"El rango etario de 11 a 20 es de "<<r20<<endl;
+    cout<<"El rango etario de 21 a 30 es de "<<r30<<endl;
+    cout<<"El rango etario de 31 a 40 es de "<<r40<<endl;
+    cout<<"El rango etario de 41 a 50 es de "<<r50<<endl;
+    cout<<"El rango etario de 510 a 60 es de "<<r60<<endl;
 }
 
 int funcestad(string fileName)
 {
-    int cinfectados=-1, cmuertes=-1,casos4050=-1,casos010=-1,rango10=0;
+    int cinfectados=-1, cmuertes=-1,casos4050=-1,rango10=0,rango20=0,rango30=0,rango40=0,rango50=0,rango60=0,rango70=0,rango80=0,rango90=0,rango100=0,rango110=0;
     fstream fin;
     fin.open("./" + fileName, ios::in);
 
@@ -82,10 +90,63 @@ int funcestad(string fileName)
             }
         string años;
 
+        if (row.getDato(20).compare("Confirmado") == 0 || total==0)
+        {
+            if (row.getDato(2).compare("NA") != 0)
+            {
+                int edad=0;
+                stringstream(row.getDato(2))>>edad;
+
+                if (edad>0 || edad <= 10)
+                {
+                    rango10 ++;
+                }
+                if (edad>= 11 || edad >=20)
+                {
+                    rango20++;
+                }
+                if (edad>= 21 || edad >=30)
+                {
+                    rango30++;
+                }
+                if (edad>= 31 || edad >=40)
+                {
+                    rango40++;
+                }
+                if (edad>=41 || edad >=50)
+                {
+                    rango50++;
+                }
+                if (edad>= 51 || edad >=60)
+                {
+                    rango60++;
+                }
+                if (edad>= 61 || edad >=70)
+                {
+                    rango70++;
+                }
+                if (edad>= 71 || edad >=80)
+                {
+                    rango80++;
+                }
+                if (edad>= 81 || edad >=90)
+                {
+                    rango90++;
+                }
+                if (edad>= 91 || edad >=100)
+                {
+                    rango100++;
+                }
+                if (edad>= 101 || edad >=110)
+                {
+                    rango110++;
+                }
+            }
+        }
 
     }
     cout<<"rango 10 "<<rango10<<endl;
-    estad(total,cinfectados,cmuertes);
+    estad(total,cinfectados,cmuertes,rango10,rango20,rango30,rango40,rango50,rango60,rango70,rango80,rango90,rango100,rango110);
     return 0;
 }
 //termina estad
@@ -173,7 +234,7 @@ int funcmuertes(string fileName)
     int colsOfInterest[] = {5};
     int nColumns = sizeof(colsOfInterest) / sizeof(colsOfInterest[0]);
 
-    int muertescaba = -1;
+    int muertescaba =-1;
     Pila <string> p;
     fstream fin;
     fin.open("./" + fileName, ios::in);
@@ -220,7 +281,6 @@ int funcmuertes(string fileName)
     //cout<<muertescaba;
 
     //return muertescaba;
-
 }
 
 
@@ -272,9 +332,9 @@ int main(int argc, char **argv)
             cout << "Nombre del Archivo: " << argv[i+1] << endl;
             //exploreHeaders(argv[i+1]);
            // exploreCSV(argv[i+1]);
-          p_casos(argv[i+1]);
+          //p_casos(argv[i+1]);
           //funcmuertes(argv[i+1]);
-          //funcestad(argv[i+1]);
+          funcestad(argv[i+1]);
             break;
         }
     }
